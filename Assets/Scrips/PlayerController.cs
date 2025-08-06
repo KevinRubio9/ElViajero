a7964ee7099c51ba31e7ae3567c899966eaa63a1
Efrain500 <maiquel50@hotmail.com>
2025-08-06 17:33:44 -0500

    Script movement player

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    CharacterController character;
+    private float movHori;
+    private float movVert;
+    public float speed;
+    private float gravity = -9.81f;
+
+    private void Awake()
     {
         
+    }
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // movimiento eje X y Z
+        movHori = Input.GetAxis("Horizontal");
+        movVert = Input.GetAxis("Vertical");
+        Vector3 mov = new Vector3(movHori, 0, movVert);
+
+       character.Move(mov * speed*Time.deltaTime);
+
+
     }
 }
