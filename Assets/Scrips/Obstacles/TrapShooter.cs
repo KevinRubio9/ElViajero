62406f64216348f9412bf9faf529224e8677e4c6
Daniel <maiquel50@hotmail.com>
2025-09-05 17:55:00 -0500

    create prefab trap shooter

--- Assets/Scrips/Obstacles/TrapShooter.cs
+++ Assets/Scrips/Obstacles/TrapShooter.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class TrapShooter : MonoBehaviour
 {
+    public GameObject prefebBullet;
+    public float rateShoot;
+    public List<Transform> pointShoot;
+    float timerShoot;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; pointShoot.Count < i; i++)
+        {
+            if (Time.time >= timerShoot)
+            {
+                Instantiate(prefebBullet, pointShoot[i].position, pointShoot[i].rotation);
+                timerShoot = Time.time + rateShoot;
+            }
+        }
     }
 }
