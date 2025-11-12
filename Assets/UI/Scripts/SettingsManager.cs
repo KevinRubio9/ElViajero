d01da9c00d0045faa2c23fc991632552e4862a0b
alejaa13 <aleja.jimenezz13@gmail.com>
2025-11-12 14:51:17 -0500

    Options

--- Assets/UI/Scripts/SettingsManager.cs
+++ Assets/UI/Scripts/SettingsManager.cs
@@ -1,16 +1,77 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class SettingsManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Resolutions")]
+
+    public Toggle toggle;
+    public TMP_Dropdown resolutionsDropDown;
+    Resolution[] resolutions;
+
+    [Header("Volume")]
+
+    public Slider slider;
     void Start()
     {
-        
+        if (Screen.fullScreen)
+        {
+            toggle.isOn = true;
+        }
+
+        else
+        {
+            toggle.isOn = false;
+        }
+
+        CheckResolution();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void FullScreenActivated(bool fullScreen)
+    {
+        Screen.fullScreen = fullScreen;
+    }
+
+    public void CheckResolution()
+    {
+        resolutions = Screen.resolutions;
+        resolutionsDropDown.ClearOptions();
+
+        List<string> options = new List<string>();
+        int actualResolution = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (Screen.fullScreen && resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                actualResolution = i;
+            }
+        }
+
+        resolutionsDropDown.AddOptions(options);
+        resolutionsDropDown.value = actualResolution;
+        resolutionsDropDown.RefreshShownValue();
+
+        resolutionsDropDown.value = PlayerPrefs.GetInt("resolutionNumber", 0);
+    }
+
+    public void ChangeResolution(int resolutionIndex)
+    {
+        PlayerPrefs.SetInt("resolutionNumber", resolutionsDropDown.value);
+
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
